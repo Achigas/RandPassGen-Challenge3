c4dfea78311647fc92a6f5e76ae746c9acc031f6
AChigas <Achigas34@gmail.com>
2020-06-08 12:05:33 -0700

    Validated alluser responses

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,36 +2,81 @@
 
 // How many characters 8-128
 
-var Characters = function() {
-  var totalCharacters = window.prompt("How many characters would you like in the password? Please select a number between 8-128.")
-//Change string to integer 
-totalCharacters = parseInt(totalCharacters);
-    //validate response
-    if (totalCharacters >= 8 && totalCharacters <= 128) {
-       window.alert("yay" + totalCharacters) }
-    else {
-      window.alert("Please enter a number between 8 and 128");
-      Characters();
-    }
-  }
-
-  Characters();
-
-//Lowercase characters?
   //validate response
   //if True, Loop and store value(s)
   //if false, skip to next question.
 
+var NumofCharacters = function() {
+  var length = window.prompt("How many characters would you like in the password? Please select a number between 8-128.")
+//Change string to integer 
+length = parseInt(length);
+    //validate response
+    if (length >= 8 && length <= 128) {
+       return length }
+    else {
+      window.alert("Please enter a number between 8 and 128");
+      NumofCharacters();
+    }
+  }
+
+NumofCharacters();
+
+  //END CHARACTER CALL 
+//var generatePassword = function (totalCharacters) 
+//Lowercase characters?
+
+var hasLower = function() {
+  var confirmLower = window.prompt("Would you like to include lowercase characters (Yes/No)?")
+  if (confirmLower==="YES" || confirmLower==="Yes" || confirmLower==="yes") {
+    return true }
+  else if (confirmLower==="NO" || confirmLower==="No" || confirmLower==="no") {
+    return false }
+  else {
+    window.alert("Please enter Yes or No.")
+    hasLower(); }
+  }
+
 //Uppercase characters?
-  //validate response
-  //if true, loop and store value(s)
-  //if false, skip to next question. 
+var hasUpper = function () {
+  var confirmUpper= window.prompt("Would you like to include uppercase characters (Yes/No)?")
+    if (confirmUpper==="YES" || confirmUpper==="Yes" || confirmUpper==="yes") {
+      return true }
+    else if (confirmUpper==="NO" || confirmUpper==="No" || confirmUpper==="no") {
+      return false }
+    else {
+      window.alert("Please enter Yes or No.")
+      hasUpper(); }
+    }
 
 //Numeric Characters?
+var hasNumeric = function () {
+  var confirmNumeric = window.prompt("Would you like to include numeric characters (Yes/No)?")
+    if (confirmNumeric==="YES" || confirmNumeric==="Yes" || confirmNumeric==="yes") {
+      return true }
+    else if (confirmNumeric==="NO" || confirmNumeric==="No" || confirmNumeric==="no") {
+      return false }
+    else {
+      window.alert("Please enter Yes or No.")
+      hasNumeric(); }
+    }
 
 //Special characters?
+var hasSpecial = function () {
+  var confirmSpecial = window.prompt("Would you like to include special characters (Yes/No)?")
+    if (confirmSpecial==="YES" || confirmSpecial==="Yes" || confirmSpecial==="yes") {
+      return true }
+    else if (confirmSpecial==="NO" || confirmSpecial==="No" || confirmSpecial=="no") {
+      return false }
+    else {
+      window.alert("Please enter Yes or No.")
+      hasSpecial(); }
+  }
 
-//make sure one is yes, if all false, run questions again. 
+  console.log(hasLower())
+  console.log(hasUpper())
+  console.log(hasNumeric())
+  console.log(hasSpecial())
+
 
 // For loop with i=length of password
 //loop through only characters that were answered yes.
@@ -50,4 +95,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", NumofCharacters);
