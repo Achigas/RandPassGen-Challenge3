58a8d23a4270e9e542bf34c74c5450d0ec8bb305
AChigas <Achigas34@gmail.com>
2020-06-08 00:57:23 -0700

    Character count validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,41 @@
 // Assignment code here
 
+// How many characters 8-128
+
+var Characters = function() {
+  var totalCharacters = window.prompt("How many characters would you like in the password? Please select a number between 8-128.")
+//Change string to integer 
+totalCharacters = parseInt(totalCharacters);
+    //validate response
+    if (totalCharacters >= 8 && totalCharacters <= 128) {
+       window.alert("yay" + totalCharacters) }
+    else {
+      window.alert("Please enter a number between 8 and 128");
+      Characters();
+    }
+  }
+
+  Characters();
+
+//Lowercase characters?
+  //validate response
+  //if True, Loop and store value(s)
+  //if false, skip to next question.
+
+//Uppercase characters?
+  //validate response
+  //if true, loop and store value(s)
+  //if false, skip to next question. 
+
+//Numeric Characters?
+
+//Special characters?
+
+//make sure one is yes, if all false, run questions again. 
+
+// For loop with i=length of password
+//loop through only characters that were answered yes.
+//random functions to assign which to use
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
